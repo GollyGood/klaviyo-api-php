7bb18c0eca6f038ecfdc36b62d00be4ba118cd0b
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2016-06-24 06:35:51 -0400

    Adding Sami configuration

---
+++ .sami-config.php
@@ -0,0 +1,22 @@
+<?php
+
+use Sami\Sami;
+use Sami\RemoteRepository\GitHubRemoteRepository;
+use Sami\Version\GitVersionCollection;
+
+$directory = __DIR__ . '/src';
+
+$versions = GitVersionCollection::create($directory)
+  ->add('develop', 'develop branch');
+
+$sami = new Sami($directory, [
+  'theme'                => 'default',
+  'versions'             => $versions,
+  'title'                => 'Klaviyo PHP API Library',
+  'build_dir'            => __DIR__ . '/../build/klaviyo-api-php/%version%',
+  'cache_dir'            => __DIR__ . '/../cache/klaviyo-api-php/%version%',
+  'remote_repository'    => new GitHubRemoteRepository('GollyGood/klaviyo-api-php', dirname($directory)),
+  'default_opened_level' => 1,
+]);
+
+return $sami;
