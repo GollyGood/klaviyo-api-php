fcef68cb8114cf3e44d6d64562d5af7e2c4a3ad9
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2016-06-24 06:51:36 -0400

    Adjusting build destinations

--- .sami-config.php
+++ .sami-config.php
@@ -13,8 +13,8 @@ $sami = new Sami($directory, [
   'theme'                => 'default',
   'versions'             => $versions,
   'title'                => 'Klaviyo PHP API Library',
-  'build_dir'            => __DIR__ . '/../build/klaviyo-api-php/%version%',
-  'cache_dir'            => __DIR__ . '/../cache/klaviyo-api-php/%version%',
+  'build_dir'            => __DIR__ . '/api/%version%',
+  'cache_dir'            => '/tmp/cache/klaviyo-api-php/%version%',
   'remote_repository'    => new GitHubRemoteRepository('GollyGood/klaviyo-api-php', dirname($directory)),
   'default_opened_level' => 1,
 ]);
