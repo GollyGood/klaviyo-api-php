a8e0e8b8e6f2bdaabdb90f1ace3251b94ac6040d
Andreas Sundqvist <andreas@forme.se>
2018-02-15 21:55:49 +0100

    Add all endpoints to TemplateService
    Add sendTemplate
    Add cloneTemplate
    Add deleteTemplate
    Add createTemplate
    Add renderTemplate
    Add getAllTemplates

--- src/TemplateService.php
+++ src/TemplateService.php
@@ -3,6 +3,7 @@
 namespace Klaviyo;
 
 use Klaviyo\Model\ModelFactory;
+use Klaviyo\Model\RenderedTemplateModel;
 use Klaviyo\Model\TemplateModel;
 
 /**
@@ -10,6 +11,26 @@ use Klaviyo\Model\TemplateModel;
  */
 class TemplateService extends BaseService
 {
+    use PagerTrait;
+
+
+    /**
+     * Retrieve all lists from Klaviyo.
+     *
+     * @return array        An array of TemplateModels that represent all templates in Klaviyo.
+     * @throws Exception\ApiConnectionException
+     * @throws Exception\BadRequestApiException
+     * @throws Exception\MissingModelTypeException
+     * @throws Exception\NotAuthorizedApiException
+     * @throws Exception\NotFoundApiException
+     * @throws Exception\ServerErrorApiException
+     */
+    public function getAllTemplates(): array
+    {
+        return $this->getAllRecords($this->getResourcePath('email-templates'));
+    }
+
+
     /**
      * Update the specified template.
      *
@@ -18,11 +39,144 @@ class TemplateService extends BaseService
      *
      * @return TemplateModel
      *   The updated template object.
+     * @throws Exception\ApiConnectionException
+     * @throws Exception\BadRequestApiException
+     * @throws Exception\MissingModelTypeException
+     * @throws Exception\NotAuthorizedApiException
+     * @throws Exception\NotFoundApiException
+     * @throws Exception\ServerErrorApiException
      */
-    public function updateTemplate(TemplateModel $template)
+    public function updateTemplate(TemplateModel $template): TemplateModel
     {
         $options = ['name' => $template->name, 'html' => $template->html->saveHtml()];
         $response = $this->api->request('PUT', $this->getResourcePath("email-template/{$template->id}"), $options);
         return ModelFactory::createFromJson($response->getBody()->getContents(), 'email-template');
+    }
+
+
+    /**
+     * Create a new template.
+     *
+     * @param string $name          The name of the template to be created.
+     * @param \DOMDocument $html    The HTML content for this template.
+     *
+     * @return TemplateModel        The template object created.
+     *
+     * @throws Exception\ApiConnectionException
+     * @throws Exception\BadRequestApiException
+     * @throws Exception\MissingModelTypeException
+     * @throws Exception\NotAuthorizedApiException
+     * @throws Exception\NotFoundApiException
+     * @throws Exception\ServerErrorApiException
+     */
+    public function createTemplate(string $name, \DOMDocument $html): TemplateModel
+    {
+        $options = [
+            'name' => $name,
+            'html' => $html->saveHTML()
+        ];
+        $response = $this->api->request('POST', $this->getResourcePath('email-templates'), $options);
+        return ModelFactory::createFromJson($response->getBody()->getContents(), 'email-template');
+    }
+
+
+    /**
+     * Delete an existing template.
+     *
+     * @param TemplateModel $template   The template object to be deleted.
+     *
+     * @return TemplateModel        The deleted template object.
+     *
+     * @throws Exception\ApiConnectionException
+     * @throws Exception\BadRequestApiException
+     * @throws Exception\MissingModelTypeException
+     * @throws Exception\NotAuthorizedApiException
+     * @throws Exception\NotFoundApiException
+     * @throws Exception\ServerErrorApiException
+     */
+    public function deleteTemplate(TemplateModel $template): TemplateModel
+    {
+        $response = $this->api->request('DELETE', $this->getResourcePath("email-template/{$template->id}"));
+        return ModelFactory::createFromJson($response->getBody()->getContents(), 'email-template');
+    }
+
+    /**
+     * @param TemplateModel $template The name of the new email template.
+     * @param string $name                  The name of the new email template.
+     *
+     * @return TemplateModel                The newly created Email Template object with summary information.
+     *
+     * @throws Exception\ApiConnectionException
+     * @throws Exception\BadRequestApiException
+     * @throws Exception\MissingModelTypeException
+     * @throws Exception\NotAuthorizedApiException
+     * @throws Exception\NotFoundApiException
+     * @throws Exception\ServerErrorApiException
+     */
+    public function cloneTemplate(TemplateModel $template, string $name): TemplateModel
+    {
+        $response = $this->api->request('POST', $this->getResourcePath("email-template/{$template->id}/clone"), [
+            'name' => $name
+        ]);
+        return ModelFactory::createFromJson($response->getBody()->getContents(), 'email-template');
+    }
+
+    /**
+     * @param TemplateModel $template
+     * @param array $context    JSON encoded hashThis is the context your email template will be rendered with.
+     *                          Email templates are rendered with contexts in a similar manner to how Django templates
+     *                          are rendered. This means that nested template variables can be referenced via dot
+     *                          notation and template variables without corresponding context values are treated as
+     *                          falsely and output nothing.
+     *
+     * @return RenderedTemplateModel
+     * @throws Exception\ApiConnectionException
+     * @throws Exception\BadRequestApiException
+     * @throws Exception\MissingModelTypeException
+     * @throws Exception\NotAuthorizedApiException
+     * @throws Exception\NotFoundApiException
+     * @throws Exception\ServerErrorApiException
+     */
+    public function renderTemplate(TemplateModel $template, array $context = []): RenderedTemplateModel
+    {
+        $response = $this->api->request('POST', $this->getResourcePath("email-template/{$template->id}/render"), [
+            'context' => json_encode($context)
+        ]);
+        return ModelFactory::createFromJson($response->getBody()->getContents(), 'rendered-template');
+    }
+
+    /**
+     * @param TemplateModel $template
+     * @param string $fromEmail The email address your email will be sent from and will be used in the reply-to header.
+     * @param string $fromName The name or label associated with the email address you're sending from.
+     * @param string $subject Subject of email
+     * @param array $to Array with email/name pairs
+     * @param array $context JSON encoded hashThis is the context your email template will be rendered with.
+     *                          Email templates are rendered with contexts in a similar manner to how Django templates
+     *                          are rendered. This means that nested template variables can be referenced via dot
+     *                          notation and template variables without corresponding context values are treated as
+     *                          falsely and output nothing.
+     * @return bool
+     *
+     * @throws Exception\ApiConnectionException
+     * @throws Exception\BadRequestApiException
+     * @throws Exception\NotAuthorizedApiException
+     * @throws Exception\NotFoundApiException
+     * @throws Exception\ServerErrorApiException
+     */
+    public function sendTemplate(TemplateModel $template, string $fromEmail, string $fromName, string $subject, array $to, array $context = []): bool
+    {
+        $response = $this->api->request('POST', $this->getResourcePath("email-template/{$template->id}/send"), [
+            'from_email' => $fromEmail,
+            'from_name' => $fromName,
+            'subject' => $subject,
+            'to' => json_encode($to),
+            'context' => json_encode($context)
+        ]);
+
+        $body = json_decode($response->getBody(), true);
+
+
+        return $body && $body['data']['status'] === 'queued';
     }
 }
--- tests/TemplateServiceTest.php
+++ tests/TemplateServiceTest.php
@@ -4,8 +4,10 @@ namespace Klaviyo\Tests;
 
 use GuzzleHttp\Psr7\Response;
 use Klaviyo\KlaviyoApi;
+use Klaviyo\Model\RenderedTemplateModel;
 use Klaviyo\Model\TemplateModel;
 use Klaviyo\TemplateService;
+use Psr\Http\Message\RequestInterface;
 
 class TemplateServiceTest extends KlaviyoTestCase
 {
@@ -17,6 +19,100 @@ class TemplateServiceTest extends KlaviyoTestCase
         'created' => '2013-06-17 14:00:00',
         'updated' => '2013-06-17 14:00:00',
     ];
+    protected $responseTemplateZero;
+    protected $responseTemplateCloned;
+    protected $responseTemplateSent;
+    protected $sendMailData;
+    protected $renderedContext = [
+        'email' => 'test@phpunit'
+    ];
+    protected $responseTemplateRendered;
+    protected $responsePageZero;
+    protected $responsePageOne;
+
+    public function setUp()
+    {
+        $this->responseTemplateZero = [
+            'object' => 'email-template',
+            'id' => 'arY8wg',
+            'name' => 'Template 1',
+            'html' => '<html><body><p>This is an email for {{ email }}.</p></body></html>',
+            'created' => '2016-01-01 18:58:54',
+            'updated' => '2016-01-02 06:00:00',
+        ];
+
+        $this->responseTemplateCloned = [
+            'object' => 'email-template',
+            'id' => 'arD8og',
+            'name' => 'Cloned template',
+            'html' => '<html><body><p>This is an email for {{ email }}.</p></body></html>',
+            'created' => '2018-01-01 18:58:54',
+            'updated' => '2018-01-02 06:00:00',
+        ];
+
+        $this->responseTemplateSent = [
+            'object' => 'email-template',
+            'id' => $this->responseTemplateZero['id'],
+            'name' => $this->responseTemplateZero['name'],
+            'data' => [
+                'status' => 'queued'
+            ]
+        ];
+
+        $this->responseTemplateRendered = [
+            "object" => "email-template",
+            "id" => "dqQnNW",
+            "name" => "Weekly Summary",
+            "data" => [
+                'html' => '<html><body><p>This is an email for '.$this->renderedContext['email'].'.</p></body></html>',
+                'text' => 'This is an email for '.$this->renderedContext['email'].'.'
+            ]
+        ];
+
+        $this->sendMailData = [
+            'from_name' => 'Test sender',
+            'from_email' => 'test@example.com',
+            'subject' => "Test subject",
+            'to' => [
+                ['email' => 'recipient@example.com', 'name' => "Recipient"]
+            ],
+            'context' => [
+                'name' => 'Recipient Name'
+            ]
+        ];
+
+        $this->responsePageZero = [
+            'object' => '$list',
+            'start' => 0,
+            'end' => 1,
+            'page_size' => 2,
+            'total' => 4,
+            'page' => 0,
+            'data' => [
+                $this->responseTemplateZero,
+                $this->responseTemplateCloned,
+            ],
+        ];
+        $this->responsePageOne = $this->responsePageZero;
+        $this->responsePageOne['start'] = 2;
+        $this->responsePageOne['end'] = 3;
+        $this->responsePageOne['page'] = 1;
+    }
+
+    public function getMultiPageListService()
+    {
+        $container = $responses = [];
+        $responses[] = new Response(200, [], json_encode($this->responsePageZero));
+        $responses[] = new Response(200, [], json_encode($this->responsePageOne));
+
+        return $this->getTemplateSerive($container, $responses);
+    }
+
+    public function getTemplateSerive(&$container, $responses): TemplateService
+    {
+        $api = $this->getApi($this->apiKey, [], $container,  $responses);
+        return new TemplateService($api);
+    }
 
     public function testUpdateTemplate()
     {
@@ -25,13 +121,185 @@ class TemplateServiceTest extends KlaviyoTestCase
         $template['html'] = '<html><body>Yay it was changed.</body></html>';
         $responses[] = new Response(200, [], json_encode($template));
 
-        $api = $this->getApi($this->apiKey, [], $container,  $responses);
-        $template_service = new TemplateService($api);
+        $template_service = $this->getTemplateSerive($container, $responses);
 
         $template = TemplateModel::create($this->templateConfiguration);
         $template->html = '<html><body>Yay it was changed.</body></html>';
 
         $response = $template_service->updateTemplate($template);
         $this->assertEquals($template, $response);
+    }
+
+    public function testCreateTemplate()
+    {
+        $container = $responses = [];
+        $responses[] = new Response(200, [], json_encode($this->responseTemplateZero));
+        $templateService = $this->getTemplateSerive($container, $responses);
+
+
+        $html = new \DOMDocument();
+        @$html->loadHTML($this->responseTemplateZero['html'], LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+        $newTemplate = $templateService->createTemplate($this->responseTemplateZero['name'], $html);
+
+        $templateZero = new TemplateModel($this->responseTemplateZero);
+        $this->assertEquals($templateZero, $newTemplate);
+
+        /** @var RequestInterface $request */
+        $request = $container[0]['request'];
+
+        $this->assertSame('POST', $request->getMethod());
+
+        $fields = array();
+        parse_str(urldecode((string) $request->getBody()), $fields);
+        $this->assertSame(
+            $this->responseTemplateZero['name'],
+            $fields['name']
+        );
+        $this->assertSame(
+            $this->responseTemplateZero['html'],
+            trim($fields['html'])
+        );
+        $this->assertSame($this->apiKey, $fields['api_key']);
+    }
+
+    public function testListTemplates()
+    {
+        $templateService = $this->getMultiPageListService();
+        $templates = $templateService->getAllTemplates();
+
+        $this->assertCount(4, $templates);
+
+        $templateZero = new TemplateModel($this->responsePageZero['data'][0]);
+        $this->assertEquals($templateZero, $templates[0]);
+        $templateOne = new TemplateModel($this->responsePageZero['data'][1]);
+        $this->assertEquals($templateOne, $templates[1]);
+        $templateTwo = new TemplateModel($this->responsePageOne['data'][0]);
+        $this->assertEquals($templateTwo, $templates[2]);
+        $templateThree = new TemplateModel($this->responsePageOne['data'][1]);
+        $this->assertEquals($templateThree, $templates[3]);
+    }
+
+    public function testCloneTemplate()
+    {
+        $container = $responses = [];
+        $responses[] = new Response(200, [], json_encode($this->responseTemplateCloned));
+        $templateService = $this->getTemplateSerive($container, $responses);
+
+        $templateZero = new TemplateModel($this->responseTemplateZero);
+
+        $newTemplate = $templateService->cloneTemplate($templateZero, $this->responseTemplateCloned['name']);
+
+        $templateCloned = new TemplateModel($this->responseTemplateCloned);
+        $this->assertEquals($templateCloned, $newTemplate);
+
+        /** @var RequestInterface $request */
+        $request = $container[0]['request'];
+
+        $this->assertSame('POST', $request->getMethod());
+
+        $fields = array();
+        parse_str(urldecode((string) $request->getBody()), $fields);
+        $this->assertSame(
+            $this->responseTemplateCloned['name'],
+            $fields['name']
+        );
+        $this->assertSame($this->apiKey, $fields['api_key']);
+    }
+
+    public function testDeleteTemplate()
+    {
+        $container = $responses = [];
+        $responses[] = new Response(200, [], json_encode($this->responseTemplateZero));
+        $templateSerive = $this->getTemplateSerive($container, $responses);
+
+        $template = new TemplateModel($this->responseTemplateZero);
+        $template = $templateSerive->deleteTemplate($template);
+
+        $templateZero = new TemplateModel($this->responseTemplateZero);
+        $this->assertEquals($templateZero, $template);
+
+        /** @var RequestInterface $request */
+        $request = $container[0]['request'];
+        $this->assertSame('DELETE', $request->getMethod());
+
+        parse_str(urldecode((string) $request->getBody()), $fields);
+        $this->assertSame($this->apiKey, $fields['api_key']);
+    }
+
+    public function testRenderTemplate()
+    {
+        $container = $responses = [];
+        $responses[] = new Response(200, [], json_encode($this->responseTemplateRendered));
+        $templateService = $this->getTemplateSerive($container, $responses);
+
+        $templateZero = new TemplateModel($this->responseTemplateZero);
+
+        $renderedModel = $templateService->renderTemplate($templateZero, $this->renderedContext);
+
+        $renderedTemplate = new RenderedTemplateModel($this->responseTemplateRendered);
+        $this->assertEquals($renderedTemplate, $renderedModel);
+
+        /** @var RequestInterface $request */
+        $request = $container[0]['request'];
+
+        $this->assertSame('POST', $request->getMethod());
+
+        $fields = array();
+        parse_str(urldecode((string) $request->getBody()), $fields);
+
+        $this->assertSame(
+            json_encode($this->renderedContext),
+            $fields['context']
+        );
+        $this->assertSame($this->apiKey, $fields['api_key']);
+    }
+
+    public function testSendTemplate()
+    {
+        $container = $responses = [];
+        $responses[] = new Response(200, [], json_encode($this->responseTemplateSent));
+        $templateService = $this->getTemplateSerive($container, $responses);
+
+        $templateZero = new TemplateModel($this->responseTemplateZero);
+
+        $rs = $templateService->sendTemplate(
+            $templateZero,
+            $this->sendMailData['from_email'],
+            $this->sendMailData['from_name'],
+            $this->sendMailData['subject'],
+            $this->sendMailData['to'],
+            $this->sendMailData['context']
+        );
+
+        $this->assertTrue($rs);
+
+        /** @var RequestInterface $request */
+        $request = $container[0]['request'];
+
+        $this->assertSame('POST', $request->getMethod());
+
+        $fields = array();
+        parse_str(urldecode((string) $request->getBody()), $fields);
+        $this->assertSame(
+            $this->sendMailData['from_email'],
+            $fields['from_email']
+        );
+        $this->assertSame(
+            $this->sendMailData['from_name'],
+            $fields['from_name']
+        );
+        $this->assertSame(
+            $this->sendMailData['subject'],
+            $fields['subject']
+        );
+        $this->assertSame(
+            json_encode($this->sendMailData['to']),
+            $fields['to']
+        );
+        $this->assertSame(
+            json_encode($this->sendMailData['context']),
+            $fields['context']
+        );
+        $this->assertSame($this->apiKey, $fields['api_key']);
     }
 }
